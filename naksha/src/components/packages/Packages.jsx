1805329360153a3defcf8ad20dc175e33cc2a0cf
amruthesh <amrutheshamru16@gmail.com>
2023-11-28 19:21:28 +0530

    a

--- naksha/src/components/packages/Packages.jsx
+++ naksha/src/components/packages/Packages.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import './packages.scss'
 import Navbar from '../navbar/Navbar'
 function Packages() {
@@ -6,23 +5,19 @@ function Packages() {
     <>
       <Navbar/>
       <div className='heading'>
-
         <h1 >
         our packages
       </h1>
       </div>
 
-      <section className="package_details_section">
-
-      
-
+  <section className="package_details_section">
       <div className="package_cards">
           <div className="package_card_container">
           <div className="card">
           <h1>Premium Home</h1>
               <h1>1799 /-* Sq Ft.</h1>
               </div>
-              <details>
+    <details>
 	<summary>
 		<span className="summary-title">Design details</span>
 		<div className="summary-chevron-up">
@@ -40,7 +35,7 @@ function Packages() {
   <i className="fa-solid fa-chevron-down"></i>
 		</div>
   </details>
-              <details>
+  <details>
 	<summary>
 		<span className="summary-title">Design details</span>
 		<div className="summary-chevron-up">
@@ -58,7 +53,380 @@ function Packages() {
   <i className="fa-solid fa-chevron-down"></i>
 		</div>
   </details>
+  <details>
+	<summary>
+		<span className="summary-title">Design details</span>
+		<div className="summary-chevron-up">
+    <i className="fa-solid fa-chevron-up"></i>
+		</div>
+	</summary>
+
+	<div className="summary-content">
+    <li>Working drawing for execution</li>
+    <li>Schedule of openings. (Doors and windows sizes)</li>
+    <li>Sectional Drawings</li>
+    <li>MEP (Plumbing & Electrical) Drawings</li>
+  </div>
+	<div className="summary-chevron-down">
+  <i className="fa-solid fa-chevron-down"></i>
+		</div>
+  </details>
+  <details>
+	<summary>
+		<span className="summary-title">Design details</span>
+		<div className="summary-chevron-up">
+    <i className="fa-solid fa-chevron-up"></i>
+		</div>
+	</summary>
+
+	<div className="summary-content">
+    <li>Working drawing for execution</li>
+    <li>Schedule of openings. (Doors and windows sizes)</li>
+    <li>Sectional Drawings</li>
+    <li>MEP (Plumbing & Electrical) Drawings</li>
+  </div>
+	<div className="summary-chevron-down">
+  <i className="fa-solid fa-chevron-down"></i>
+		</div>
+  </details>
+  <details>
+	<summary>
+		<span className="summary-title">Design details</span>
+		<div className="summary-chevron-up">
+    <i className="fa-solid fa-chevron-up"></i>
+		</div>
+	</summary>
+
+	<div className="summary-content">
+    <li>Working drawing for execution</li>
+    <li>Schedule of openings. (Doors and windows sizes)</li>
+    <li>Sectional Drawings</li>
+    <li>MEP (Plumbing & Electrical) Drawings</li>
+  </div>
+	<div className="summary-chevron-down">
+  <i className="fa-solid fa-chevron-down"></i>
+		</div>
+  </details>
+  <details>
+	<summary>
+		<span className="summary-title">Design details</span>
+		<div className="summary-chevron-up">
+    <i className="fa-solid fa-chevron-up"></i>
+		</div>
+	</summary>
+
+	<div className="summary-content">
+    <li>Working drawing for execution</li>
+    <li>Schedule of openings. (Doors and windows sizes)</li>
+    <li>Sectional Drawings</li>
+    <li>MEP (Plumbing & Electrical) Drawings</li>
+  </div>
+	<div className="summary-chevron-down">
+  <i className="fa-solid fa-chevron-down"></i>
+		</div>
+  </details>
+  <details>
+	<summary>
+		<span className="summary-title">Design details</span>
+		<div className="summary-chevron-up">
+    <i className="fa-solid fa-chevron-up"></i>
+		</div>
+	</summary>
+
+	<div className="summary-content">
+    <li>Working drawing for execution</li>
+    <li>Schedule of openings. (Doors and windows sizes)</li>
+    <li>Sectional Drawings</li>
+    <li>MEP (Plumbing & Electrical) Drawings</li>
+  </div>
+	<div className="summary-chevron-down">
+  <i className="fa-solid fa-chevron-down"></i>
+		</div>
+  </details>
+  <details>
+	<summary>
+		<span className="summary-title">Design details</span>
+		<div className="summary-chevron-up">
+    <i className="fa-solid fa-chevron-up"></i>
+		</div>
+	</summary>
+
+	<div className="summary-content">
+    <li>Working drawing for execution</li>
+    <li>Schedule of openings. (Doors and windows sizes)</li>
+    <li>Sectional Drawings</li>
+    <li>MEP (Plumbing & Electrical) Drawings</li>
+  </div>
+	<div className="summary-chevron-down">
+  <i className="fa-solid fa-chevron-down"></i>
+		</div>
+  </details>
+  <details>
+	<summary>
+		<span className="summary-title">Design details</span>
+		<div className="summary-chevron-up">
+    <i className="fa-solid fa-chevron-up"></i>
+		</div>
+	</summary>
+
+	<div className="summary-content">
+    <li>Working drawing for execution</li>
+    <li>Schedule of openings. (Doors and windows sizes)</li>
+    <li>Sectional Drawings</li>
+    <li>MEP (Plumbing & Electrical) Drawings</li>
+  </div>
+	<div className="summary-chevron-down">
+  <i className="fa-solid fa-chevron-down"></i>
+		</div>
+  </details>
+  <details>
+	<summary>
+		<span className="summary-title">Design details</span>
+		<div className="summary-chevron-up">
+    <i className="fa-solid fa-chevron-up"></i>
+		</div>
+	</summary>
+
+	<div className="summary-content">
+    <li>Working drawing for execution</li>
+    <li>Schedule of openings. (Doors and windows sizes)</li>
+    <li>Sectional Drawings</li>
+    <li>MEP (Plumbing & Electrical) Drawings</li>
+  </div>
+	<div className="summary-chevron-down">
+  <i className="fa-solid fa-chevron-down"></i>
+		</div>
+  </details>
+              
+
+  
             </div>
+
+            <div className="package_card_container">
+          <div className="card">
+          <h1>Luxury Home</h1>
+              <h1>2199 /-* Sq Ft.</h1>
+              </div>
+    <details>
+	<summary>
+		<span className="summary-title">Design details</span>
+		<div className="summary-chevron-up">
+    <i className="fa-solid fa-chevron-up"></i>
+		</div>
+	</summary>
+
+	<div className="summary-content">
+    <li>Working drawing for execution</li>
+    <li>Schedule of openings. (Doors and windows sizes)</li>
+    <li>Sectional Drawings</li>
+    <li>MEP (Plumbing & Electrical) Drawings</li>
+  </div>
+	<div className="summary-chevron-down">
+  <i className="fa-solid fa-chevron-down"></i>
+		</div>
+  </details>
+  <details>
+	<summary>
+		<span className="summary-title">Design details</span>
+		<div className="summary-chevron-up">
+    <i className="fa-solid fa-chevron-up"></i>
+		</div>
+	</summary>
+
+	<div className="summary-content">
+    <li>Working drawing for execution</li>
+    <li>Schedule of openings. (Doors and windows sizes)</li>
+    <li>Sectional Drawings</li>
+    <li>MEP (Plumbing & Electrical) Drawings</li>
+  </div>
+	<div className="summary-chevron-down">
+  <i className="fa-solid fa-chevron-down"></i>
+		</div>
+  </details>
+  <details>
+	<summary>
+		<span className="summary-title">Design details</span>
+		<div className="summary-chevron-up">
+    <i className="fa-solid fa-chevron-up"></i>
+		</div>
+	</summary>
+
+	<div className="summary-content">
+    <li>Working drawing for execution</li>
+    <li>Schedule of openings. (Doors and windows sizes)</li>
+    <li>Sectional Drawings</li>
+    <li>MEP (Plumbing & Electrical) Drawings</li>
+  </div>
+	<div className="summary-chevron-down">
+  <i className="fa-solid fa-chevron-down"></i>
+		</div>
+  </details>
+  <details>
+	<summary>
+		<span className="summary-title">Design details</span>
+		<div className="summary-chevron-up">
+    <i className="fa-solid fa-chevron-up"></i>
+		</div>
+	</summary>
+
+	<div className="summary-content">
+    <li>Working drawing for execution</li>
+    <li>Schedule of openings. (Doors and windows sizes)</li>
+    <li>Sectional Drawings</li>
+    <li>MEP (Plumbing & Electrical) Drawings</li>
+  </div>
+	<div className="summary-chevron-down">
+  <i className="fa-solid fa-chevron-down"></i>
+		</div>
+  </details>
+  <details>
+	<summary>
+		<span className="summary-title">Design details</span>
+		<div className="summary-chevron-up">
+    <i className="fa-solid fa-chevron-up"></i>
+		</div>
+	</summary>
+
+	<div className="summary-content">
+    <li>Working drawing for execution</li>
+    <li>Schedule of openings. (Doors and windows sizes)</li>
+    <li>Sectional Drawings</li>
+    <li>MEP (Plumbing & Electrical) Drawings</li>
+  </div>
+	<div className="summary-chevron-down">
+  <i className="fa-solid fa-chevron-down"></i>
+		</div>
+  </details>
+  <details>
+	<summary>
+		<span className="summary-title">Design details</span>
+		<div className="summary-chevron-up">
+    <i className="fa-solid fa-chevron-up"></i>
+		</div>
+	</summary>
+
+	<div className="summary-content">
+    <li>Working drawing for execution</li>
+    <li>Schedule of openings. (Doors and windows sizes)</li>
+    <li>Sectional Drawings</li>
+    <li>MEP (Plumbing & Electrical) Drawings</li>
+  </div>
+	<div className="summary-chevron-down">
+  <i className="fa-solid fa-chevron-down"></i>
+		</div>
+  </details>
+  <details>
+	<summary>
+		<span className="summary-title">Design details</span>
+		<div className="summary-chevron-up">
+    <i className="fa-solid fa-chevron-up"></i>
+		</div>
+	</summary>
+
+	<div className="summary-content">
+    <li>Working drawing for execution</li>
+    <li>Schedule of openings. (Doors and windows sizes)</li>
+    <li>Sectional Drawings</li>
+    <li>MEP (Plumbing & Electrical) Drawings</li>
+  </div>
+	<div className="summary-chevron-down">
+  <i className="fa-solid fa-chevron-down"></i>
+		</div>
+  </details>
+  <details>
+	<summary>
+		<span className="summary-title">Design details</span>
+		<div className="summary-chevron-up">
+    <i className="fa-solid fa-chevron-up"></i>
+		</div>
+	</summary>
+
+	<div className="summary-content">
+    <li>Working drawing for execution</li>
+    <li>Schedule of openings. (Doors and windows sizes)</li>
+    <li>Sectional Drawings</li>
+    <li>MEP (Plumbing & Electrical) Drawings</li>
+  </div>
+	<div className="summary-chevron-down">
+  <i className="fa-solid fa-chevron-down"></i>
+		</div>
+  </details>
+  <details>
+	<summary>
+		<span className="summary-title">Design details</span>
+		<div className="summary-chevron-up">
+    <i className="fa-solid fa-chevron-up"></i>
+		</div>
+	</summary>
+
+	<div className="summary-content">
+    <li>Working drawing for execution</li>
+    <li>Schedule of openings. (Doors and windows sizes)</li>
+    <li>Sectional Drawings</li>
+    <li>MEP (Plumbing & Electrical) Drawings</li>
+  </div>
+	<div className="summary-chevron-down">
+  <i className="fa-solid fa-chevron-down"></i>
+		</div>
+  </details>
+  <details>
+	<summary>
+		<span className="summary-title">Design details</span>
+		<div className="summary-chevron-up">
+    <i className="fa-solid fa-chevron-up"></i>
+		</div>
+	</summary>
+
+	<div className="summary-content">
+    <li>Working drawing for execution</li>
+    <li>Schedule of openings. (Doors and windows sizes)</li>
+    <li>Sectional Drawings</li>
+    <li>MEP (Plumbing & Electrical) Drawings</li>
+  </div>
+	<div className="summary-chevron-down">
+  <i className="fa-solid fa-chevron-down"></i>
+		</div>
+  </details>
+  <details>
+	<summary>
+		<span className="summary-title">Design details</span>
+		<div className="summary-chevron-up">
+    <i className="fa-solid fa-chevron-up"></i>
+		</div>
+	</summary>
+
+	<div className="summary-content">
+    <li>Working drawing for execution</li>
+    <li>Schedule of openings. (Doors and windows sizes)</li>
+    <li>Sectional Drawings</li>
+    <li>MEP (Plumbing & Electrical) Drawings</li>
+  </div>
+	<div className="summary-chevron-down">
+  <i className="fa-solid fa-chevron-down"></i>
+		</div>
+  </details>
+  <details>
+	<summary>
+		<span className="summary-title">Design details</span>
+		<div className="summary-chevron-up">
+    <i className="fa-solid fa-chevron-up"></i>
+		</div>
+	</summary>
+
+	<div className="summary-content">
+    <li>Working drawing for execution</li>
+    <li>Schedule of openings. (Doors and windows sizes)</li>
+    <li>Sectional Drawings</li>
+    <li>MEP (Plumbing & Electrical) Drawings</li>
+  </div>
+	<div className="summary-chevron-down">
+  <i className="fa-solid fa-chevron-down"></i>
+		</div>
+  </details>
+
+  
+            </div>
+
             </div>
 
                 
